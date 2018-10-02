9ce56cc49c8baf0bf2239296894c443f05dd08f6
melissasedgwick <melissa.sedgwick@yahoo.co.uk>
2018-10-02 16:44:52 +0100

    revisited 11th exercise redux

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -1,20 +1,28 @@
 class DockingStation
   attr_reader :var
 
- def initialize
-   @var = Bike.new
-   @bikes_released = Array.new
- end
+  def initialize
+    @var = Bike.new
+    @@bikes_released = Array.new
+    @station_free = false
+  end
 
   def release_bike
     @var.working?
-    @bikes_released.push(@var)
+    @@bikes_released.push(@var)
+    @station_free = true
     return @var
   end
 
- def dock (bike)
-  @bikes_released.include?(bike) ? true : false
- end
+  def dock(bike)
+    if @station_free == true && @@bikes_released.include?(bike)
+      @@bikes_released.delete(bike)
+      @station_free = false
+      return "bike docked"
+    else
+      return "unable to dock bike"
+    end
+  end
 
 end
 
