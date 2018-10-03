6f8129bd87c947835a39d508e46623d391446453
melissasedgwick <melissa.sedgwick@yahoo.co.uk>
2018-10-03 15:15:38 +0100

    chapter 15 - SOLID and private

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -7,17 +7,36 @@ class DockingStation
   end
 
   def release_bike
-    raise "No bike available" unless @bikes_in_station.length > 0
+    #raise "No bike available" unless @bikes_in_station.length > 0
+    raise "No bike available" if empty?
     @bikes_in_station.pop
     return @bike
   end
 
   def dock(bike)
-    raise "Unable to dock bike" unless @bikes_in_station.length < @max_size
-      @bikes_in_station << @bike
-      @bike = bike
-      return "Bike docked"
+    #raise "Unable to dock bike" unless @bikes_in_station.length < @max_size
+    raise "Unable to dock bike" if full?
+    @bikes_in_station << @bike
+    return "Bike docked"
   end
+
+  def full?
+    if @bikes_in_station.length == @max_size
+      return true
+    else
+      return false
+    end
+  end
+
+  def empty?
+    if @bikes_in_station.length == 0
+      return true
+    else
+      return false
+    end
+  end
+
+  private :full?, :empty?
 
 end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -12,32 +12,34 @@ describe DockingStation do
   end
 
   it { is_expected.to respond_to(:release_bike) }
+#  it { is_expected.to respond_to(:empty?) }
+#  it { is_expected.to respond_to(:full?) }
 
   it "Release bike creating an instance of Bike Class" do
     expect(@docking_station.release_bike).to be_a(Bike)
   end
 
-     it "Returns true from Working? method" do
-       expect(@docking_station.release_bike.working?).to eq(true)
-     end
+  it "Returns true from Working? method" do
+    expect(@docking_station.release_bike.working?).to eq(true)
+  end
 
-     it {is_expected.to respond_to(:dock).with(1).argument}
+  it {is_expected.to respond_to(:dock).with(1).argument}
 
-     it "Calling dock bike when the station is not free returns warning" do
-       19.times do
-         @docking_station.dock(Bike.new)
-       end
-       expect{@docking_station.dock(@bike)}.to raise_error("Unable to dock bike")
-     end
+  it "Calling dock bike when the station is not free returns warning" do
+    19.times do
+      @docking_station.dock(Bike.new)
+    end
+    expect{@docking_station.dock(@bike)}.to raise_error("Unable to dock bike")
+  end
 
-     it "Releasing bike from empty station returns warning" do
-       @docking_station.release_bike
-       expect{@docking_station.release_bike}.to raise_error("No bike available")
-     end
+  it "Releasing bike from empty station returns warning" do
+    @docking_station.release_bike
+    expect{@docking_station.release_bike}.to raise_error("No bike available")
+  end
 
-     it "Calling dock bike when the station is free returns success message" do
-       bike1 = @docking_station.release_bike
-       expect(@docking_station.dock(bike1)).to eq "Bike docked"
-     end
+  it "Calling dock bike when the station is free returns success message" do
+    bike1 = @docking_station.release_bike
+    expect(@docking_station.dock(bike1)).to eq "Bike docked"
+  end
 
 end
