d0d958343ca329661678caa9aa0d063febda649f
melissasedgwick <melissa.sedgwick@yahoo.co.uk>
2018-10-03 12:11:25 +0100

    Updated variable names

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -1,29 +1,30 @@
+@@register = { bikes: [], #if there's an entry within this array, it means a bike is being used.
+stations: []#if there's an entry in this array, it means a station is empty. Not yet coded.
+}
+
 class DockingStation
   attr_reader :var
 
   def initialize
-    @var = Bike.new
-    #@@bike_created.push(@var)
-    @@bikes_released = Array.new
+    @bike = Bike.new
     @station_contains_bike = true
   end
 
   def release_bike
     if @station_contains_bike == true
-      @var.working?
-      @@bikes_released.push(@var)
+      @@register[:bikes] << @bike
       @station_contains_bike = false
-      return @var
+      return @bike
     else
       "No bike available"
     end
   end
 
   def dock(bike)
-    if @station_contains_bike == false && @@bikes_released.include?(bike)
-      @@bikes_released.delete(bike)
+    if @station_contains_bike == false && @@register[:bikes].include?(bike)
+      @@register[:bikes].delete(bike)
       @station_contains_bike = true
-      @var = bike
+      @bike = bike
       return "Bike docked"
     else
       return "Unable to dock bike"
